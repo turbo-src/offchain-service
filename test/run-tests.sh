1bc3cb9e76edcf3e8c9cd38b571b4f63a67b7b41
Jeffrey Wood <jwfirenze@gmail.com>
2022-07-25 13:08:21 -0400

    run-tests.sh initialized

---
+++ test/run-tests.sh
@@ -0,0 +1,17 @@
+npm test test/createUser.js
+
+sleep 5
+
+npm test test/createRepo.js
+
+sleep5
+
+npm test test/transferTokens.js
+
+sleep 5
+
+npm test test/createPullRequest
+
+sleep 5
+
+npm test test/twoVoters.js
