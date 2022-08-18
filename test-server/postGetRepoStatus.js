f5eec49136117494c604de4b1279723e51d54834
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-18 15:57:11 -0400

    repo instead of repo_id most of the time

--- test-server/postGetRepoStatus.js
+++ test-server/postGetRepoStatus.js
@@ -5,10 +5,10 @@ describe("postGetRepoStatus", function () {
   it("should return 200 if a repo has been tokenized", async function () {
     let repoStatus = await postGetRepoStatus("joseph/demo");
     let repoStatusNotFound = await postGetRepoStatus("jeffrey/demo42");
-    assert.equal(repoStatus, "200", "Failed to find a tokenized repo");
+    assert.equal(repoStatus, 200, "Failed to find a tokenized repo");
     assert.equal(
       repoStatusNotFound,
-      "404",
+      404,
       "This repo should return 404 as it has not been created"
     );
   });
