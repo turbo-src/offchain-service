896218c248e8823f3f30c66b7d29d3b0d45735e0
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-19 09:58:39 -0400

    tests passing

--- test-server/postGetRepoStatus.js
+++ test-server/postGetRepoStatus.js
@@ -4,12 +4,12 @@ const { postGetRepoStatus } = require("../src/requests");
 describe("postGetRepoStatus", function () {
   it("should return 200 if a repo has been tokenized", async function () {
     let repoStatus = await postGetRepoStatus("joseph/demo");
-    // let repoStatusNotFound = await postGetRepoStatus("jeffrey/demo42");
+    let repoStatusNotFound = await postGetRepoStatus("jeffrey/demo42");
     assert.equal(repoStatus, 200, "Failed to find a tokenized repo");
-    // assert.equal(
-    //   repoStatusNotFound,
-    //   404,
-    //   "This repo should return 404 as it has not been created"
-    // );
+    assert.equal(
+      repoStatusNotFound,
+      404,
+      "This repo should return 404 as it has not been created"
+    );
   });
 });
