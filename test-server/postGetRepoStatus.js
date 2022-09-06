52b0859604db747689fba15564ef9bb6f7ace5cb
7db9a <repo.7db9a@gmail.com>
2022-09-06 19:37:00 -0400

    tests: pass postGetRepoStatus cases.

--- test-server/postGetRepoStatus.js
+++ test-server/postGetRepoStatus.js
@@ -5,10 +5,14 @@ describe("postGetRepoStatus", function () {
   it("should return 200 if a repo has been tokenized", async function () {
     let repoStatus = await postGetRepoStatus("joseph/demo");
     let repoStatusNotFound = await postGetRepoStatus("jeffrey/demo42");
-    assert.equal(repoStatus, true, "Failed to find a tokenized repo");
-    assert.equal(
+    assert.deepEqual(
+      repoStatus,
+      { code: 200, result: true },
+      "Failed to find a tokenized repo"
+    );
+    assert.deepEqual(
       repoStatusNotFound,
-      false,
+      { code: 200, result: false },
       "This repo should return 404 as it has not been created"
     );
   });
