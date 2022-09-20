721b0c4ca0ce7a6fc1afe2c4badcad4060acdd52
7db9a <repo.7db9a@gmail.com>
2022-09-19 22:34:22 -0400

    test: add getPRvoteStatus test.

---
+++ test-server/getPRvoteStatus.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+const { postGetPRvoteStatus } = require("../src/requests");
+
+describe("getPRvoteStatus", function () {
+  it("should return 200 if a repo has been tokenized", async function () {
+    let prStatus = await postGetPRvoteStatus(
+	    "",
+	    "joseph/demo",
+	    "pullRequest1",
+	    "",
+	    ""
+    );
+    //let prStatusNotFound = await getPRvoteStatus("jeffrey/demo42", "issue_1");
+    assert.deepEqual(
+      prStatus,
+      {
+        status: 200,
+	type: 2
+      },
+      "Failed to find a tokenized repo"
+    );
+    //assert.deepEqual(
+    //  prStatusNotFound,
+    //  { status: 200, exists: false },
+    //  "This repo should return 404 as it has not been created"
+    //);
+  });
+});
