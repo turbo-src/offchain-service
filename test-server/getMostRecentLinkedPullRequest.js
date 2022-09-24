e9bf9681bcc2f73ee0b3b71bc544c1a4f916e720
7db9a <repo.7db9a@gmail.com>
2022-09-23 21:13:51 -0400

    tests: pass getting last pull request.

--- test-server/getMostRecentLinkedPullRequest.js
+++ test-server/getMostRecentLinkedPullRequest.js
@@ -3,12 +3,25 @@ const { postGetMostRecentLinkedPullRequest } = require("../src/requests");
 
 describe("getMostRecentLinkedPullRequest", function () {
   it("should get most recent linked pull request", async function () {
+    const pullRequestConflicts = await postGetMostRecentLinkedPullRequest(
+      /*owner:*/ "joseph",
+      /*repo:*/ "joseph/demo",
+      /*defaultHash:*/ "defaultHash8",
+      /*contributor:*/ "",
+      /*side:*/ ""
+    );
+
     const pullRequestUpdates = await postGetMostRecentLinkedPullRequest(
       /*owner:*/ "joseph",
       /*repo:*/ "joseph/demo",
       /*defaultHash:*/ "defaultHash9",
       /*contributor:*/ "",
       /*side:*/ ""
+    );
+    assert.deepEqual(
+      pullRequestConflicts,
+     { status: 200, state: "new", repo_id: "joseph/demo",  fork_branch: "pullRequest8c", "childDefaultHash": "defaultHash8c", "defaultHash": "defaultHash8c" },
+      "Fail to stay open."
     );
 
     assert.deepEqual(
