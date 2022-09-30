b74e5801747cafcbbc6b0c16f567c4782b69a27d
7db9a <repo.7db9a@gmail.com>
2022-09-29 21:39:55 -0400

    tests: vote into conflict.

--- test-server/createLinkedPullRequest.js
+++ test-server/createLinkedPullRequest.js
@@ -1,9 +1,36 @@
 const assert = require("assert");
-const { createLinkedPullRequest } = require("../src/requests");
+const {
+  postSetVote,
+  postGetMostRecentLinkedPullRequest,
+  postGetPRvoteYesTotals,
+  postGetPRvoteNoTotals,
+  createLinkedPullRequest
+} = require("../src/requests");
 
 describe("createLinkedPullRequest", function () {
   it("create a pull request associated with the repo_id supplied", async function () {
     this.timeout(4000);
+    // Vote on original
+    let michaelVote = await postSetVote(
+      /*owner:*/ "joseph",
+      /*repo:*/ "joseph/demo",
+      /*defaultHash:*/ "defaultHash10",
+      /*childDefaultHash:*/ "defaultHash10",
+      /*mergeable:*/ true,
+      /*contributor_id:*/ "0x0c55D3B26A1229B9D707a4272F55E66103301858",
+      /*side:*/ "yes"
+    );
+
+    let gabrielVote = await postSetVote(
+      /*owner:*/ "joseph",
+      /*repo:*/ "joseph/demo",
+      /*defaultHash:*/ "defaultHash10",
+      /*childDefaultHash:*/ "defaultHash10b",
+      /*mergeable:*/ false,
+      /*contributor_id:*/ "0x0cf39Fb66C908A8aAb733F52BaDbf1ED58036983",
+      /*side*/ "yes"
+    );
+
     const issue_10b = await createLinkedPullRequest(
       /*owner:*/ "joseph",
       /*repo_id:*/ "joseph/demo",
@@ -18,10 +45,38 @@ describe("createLinkedPullRequest", function () {
       /*title:*/ "feat: create linked pull request."
     );
 
+    const pullRequestLatest = await postGetMostRecentLinkedPullRequest(
+      /*owner:*/ "joseph",
+      /*repo:*/ "joseph/demo",
+      /*defaultHash:*/ "defaultHash10",
+      /*contributor:*/ "",
+      /*side:*/ ""
+    );
+    const voteYesTotals = await postGetPRvoteYesTotals(
+      /*owner:*/ "joseph",
+      /*repo:*/ "joseph/demo",
+      /*defaultHash:*/ "defaultHash1",
+      /*contributor:*/ "",
+      /*side:*/ ""
+    );
+
+    const voteNoTotalsAfterMary = await postGetPRvoteNoTotals(
+      /*owner:*/ "joseph",
+      /*repo:*/ "joseph/demo",
+      /*defaultHash:*/ "defaultHash1",
+      /*contributor:*/ "",
+      /*side:*/ ""
+    );
+
     assert.equal(
       issue_10b,
       "201",
       "Failed to create a pull request issue_10 in the database"
+    );
+    assert.deepEqual(
+      pullRequestLatest,
+     { status: 200, state: "new", repo_id: "joseph/demo",  fork_branch: "pullRequest10", "childDefaultHash": "defaultHash10b", "defaultHash": "defaultHash10b", head: "head", branchDefaultHash: "branchDefaultHash", remoteURL: "remoteURL", baseBranch: "master" },
+      "Fail to stay open."
     );
   });
 });
