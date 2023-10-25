83c61b2cfe3422db52b88233b4d118d97b03b6d2
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2023-10-25 10:32:55 -0400

    commented out code removed

--- server/db/Models/Repo.js
+++ server/db/Models/Repo.js
@@ -43,37 +43,5 @@ const initialBalance = async (Repo) => {
 	});
 };
 
-const unFreeze = async (Repo) => {
-  // const frozen = await PullRequest.findAll({
-  //   where: { repo_id: repo.repo_id, state: "frozen" },
-  // });
-  // for (let i = 0; i < frozen.length; i++) {
-  //   if (
-  //     frozen[i].dataValues.yesTokenAmount === 0 &&
-  //     frozen[i].dataValues.noTokenAmount === 0
-  //   ) {
-  //     console.log("frozen vote pr", frozen[i]);
-  //     console.log("frozen vote pr", frozen[i].dataValues);
-  //     console.log("frozen vote pr", frozen[i].dataValues.id);
-  //     await PullRequest.update({
-  //       state: "vote",
-  //       where: { id: frozen[i].dataValues.id },
-  //     });
-  //   } else {
-  //     console.log("frozen pre open pr", frozen[i]);
-  //     await PullRequest.update({
-  //       state: "pre-open",
-  //       where: { id: frozen[i].dataValues.id },
-  //     });
-  //   }
-  // }
-  console.log(Repo)
-}
-
-Repo.afterBulkUpdate(console.log('#> after bulk update'), unFreeze)
-Repo.afterUpdate(console.log('#> after update'), unFreeze)
-Repo.beforeUpdate(console.log('#> before update'), unFreeze)
-
-
 Repo.afterCreate(initialBalance);
 module.exports = Repo;
