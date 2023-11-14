9dd301e20c2cce1088dc350bd093a1a0c50d00bf
Jeffrey Wood <jeffreywood@jeffreys-air.lan>
2023-11-14 16:13:20 -0500

    state arg added in set vote

--- server/index.js
+++ server/index.js
@@ -187,7 +187,9 @@ var root = {
       args.baseBranch,
       args.fork_branch,
       args.title,
-      args.issue_id    );
+      args.issue_id,
+      args.state
+    );
   },
   createLinkedPullRequest: async (args) => {
     return await createLinkedPullRequest(
