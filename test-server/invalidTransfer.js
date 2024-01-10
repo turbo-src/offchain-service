80a07e276997ce8398303615972fcd2d0a8f08c6
7db9a <>
2024-01-10 17:15:19 -0500

    tests: pass up to duplicate vote.

--- test-server/invalidTransfer.js
+++ test-server/invalidTransfer.js
@@ -38,9 +38,50 @@ describe("Invalid transfers should return 403, valid should return 201", functio
       /*amount*/ "25"
     );
 
-    assert.equal(ignaciusTransfer, "403");
-    assert.equal(thibautTransfer, "403");
-    assert.equal(thibautTransferSuccess, "201");
-    assert.equal(ignaciusTransferSuccess, "201");
+    assert.deepEqual(
+      ignaciusTransfer,
+      {
+        amount: 1,
+        createdAt: '',
+        from: '0x0c16EFDc6e6490fd6066AB794Dc841A50eB5C90C',
+        id: '',
+        network: 'Turbosrc',
+        repo: 'joseph/demo',
+        status: 403,
+        to: '0x0c587fB3EBA5e824Df543bDE5d972Fd9F7cFC164'
+      }
+    );
+    assert.deepEqual(
+      thibautTransfer,
+      {
+        amount: 50_001,
+        createdAt: '',
+        from: '0x0c587fB3EBA5e824Df543bDE5d972Fd9F7cFC164',
+        id: '',
+        network: 'Turbosrc',
+        repo: 'joseph/demo',
+        status: 403,
+        to: '0x0c16EFDc6e6490fd6066AB794Dc841A50eB5C90C'
+      },
+      'failed to prevent invalid token transfer.'
+    )
+
+    assert.deepEqual(
+      thibautTransferSuccess.amount,
+      50
+    )
+    assert.deepEqual(
+      thibautTransferSuccess.status,
+      201
+    )
+
+    assert.deepEqual(
+      ignaciusTransferSuccess.amount,
+      25
+    )
+    assert.deepEqual(
+      ignaciusTransferSuccess.status,
+      201
+    )
   });
 });
